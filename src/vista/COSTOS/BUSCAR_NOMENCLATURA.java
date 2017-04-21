5f3619dd16ff49624d1083d075839407ac3df70b
silvana25almeyda2 <silvana_25_damaris@hotmail.com>
2017-04-21 10:27:02 -0500

    silvana

--- src/vista/COSTOS/BUSCAR_NOMENCLATURA.java
+++ src/vista/COSTOS/BUSCAR_NOMENCLATURA.java
@@ -161,11 +161,7 @@ Conexion conectar=new Conexion();
             }
         ));
         tbNomenclatura.setAutoResizeMode(javax.swing.JTable.AUTO_RESIZE_OFF);
-<<<<<<< HEAD
-        tbNomenclatura.setRowHeight(24);
-=======
         tbNomenclatura.setRowHeight(20);
->>>>>>> 174a37581c1907b2a33dee5eb20e788259eda61a
         tbNomenclatura.setSelectionBackground(new java.awt.Color(102, 102, 102));
         tbNomenclatura.addKeyListener(new java.awt.event.KeyAdapter() {
             public void keyPressed(java.awt.event.KeyEvent evt) {
@@ -201,9 +197,9 @@ Conexion conectar=new Conexion();
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addGap(180, 180, 180)
-                .addComponent(txtBuscar, javax.swing.GroupLayout.PREFERRED_SIZE, 240, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGap(199, 199, 199)
+                .addComponent(txtBuscar, javax.swing.GroupLayout.PREFERRED_SIZE, 217, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                 .addComponent(btnBuscar, javax.swing.GroupLayout.PREFERRED_SIZE, 32, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addContainerGap(176, Short.MAX_VALUE))
             .addGroup(layout.createSequentialGroup()

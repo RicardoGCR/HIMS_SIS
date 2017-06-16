1d3f3086274b55707d6b828dab6c0008253da896
Martha1604 <martha_arias@outlook.com>
2017-06-16 13:49:31 -0500

    Martha

--- src/modelos/PERSONAL/CLS_PERSONAL_ROL.java
+++ src/modelos/PERSONAL/CLS_PERSONAL_ROL.java
@@ -6,6 +6,7 @@
 package modelos.PERSONAL;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import servicios.Conexion;
 
 /**
@@ -14,11 +15,52 @@ import servicios.Conexion;
  */
 public class CLS_PERSONAL_ROL {
     private Connection cn;
+    private String COD_TUR_UO;
+    private int ID_PER_UNI_ORG;
+    private String DIA_ROL;
+    private String MES_ROL;
+    private String ANIO_ROL;
+    private int LIMITE_CONSULTA_DIA;
+    private String ENVIO_ROL;
+    private String TOTAL_HORAS;
+    private double TOTAL_PAGO;
+    private String NOM_USU;
     
     public CLS_PERSONAL_ROL()
     {
         Conexion con = new Conexion();
         cn = con.conectar();
+    }
+    
+    public boolean PERSONAL_ROL_GUARDAR(){
+        boolean resp = false;
+        try
+        {
+            String sql = "exec PERSONAL_PERSONAL_ROL_GUARDAR ?,?,?,?,?,?,?,?,?,?";
+            PreparedStatement cmd = getCn().prepareStatement(sql);
+            cmd.setString(1, getCOD_TUR_UO());
+            cmd.setInt(2, getID_PER_UNI_ORG());
+            cmd.setString(3, getDIA_ROL());
+            cmd.setString(4, getMES_ROL());
+            cmd.setString(5, getANIO_ROL());
+            cmd.setInt(6, getLIMITE_CONSULTA_DIA());          
+            cmd.setString(7, getENVIO_ROL());
+            cmd.setString(8, getTOTAL_HORAS());
+            cmd.setDouble(9, getTOTAL_PAGO());
+            cmd.setString(10, getNOM_USU());
+            
+            if(!cmd.execute())
+            {
+                resp = true;
+            }
+            cmd.close();
+            getCn().close();
+        }
+        catch(Exception ex)
+        {
+            System.out.println("Error guardar personal rol: " + ex.getMessage());
+        }
+        return resp;
     }
 
     public Connection getCn() {
@@ -28,6 +70,85 @@ public class CLS_PERSONAL_ROL {
     public void setCn(Connection cn) {
         this.cn = cn;
     }
-    
-    
+
+    public String getCOD_TUR_UO() {
+        return COD_TUR_UO;
+    }
+
+    public void setCOD_TUR_UO(String COD_TUR_UO) {
+        this.COD_TUR_UO = COD_TUR_UO;
+    }
+
+    public int getID_PER_UNI_ORG() {
+        return ID_PER_UNI_ORG;
+    }
+
+    public void setID_PER_UNI_ORG(int ID_PER_UNI_ORG) {
+        this.ID_PER_UNI_ORG = ID_PER_UNI_ORG;
+    }
+
+    public String getDIA_ROL() {
+        return DIA_ROL;
+    }
+
+    public void setDIA_ROL(String DIA_ROL) {
+        this.DIA_ROL = DIA_ROL;
+    }
+
+    public String getMES_ROL() {
+        return MES_ROL;
+    }
+
+    public void setMES_ROL(String MES_ROL) {
+        this.MES_ROL = MES_ROL;
+    }
+
+    public String getANIO_ROL() {
+        return ANIO_ROL;
+    }
+
+    public void setANIO_ROL(String ANIO_ROL) {
+        this.ANIO_ROL = ANIO_ROL;
+    }
+
+    public int getLIMITE_CONSULTA_DIA() {
+        return LIMITE_CONSULTA_DIA;
+    }
+
+    public void setLIMITE_CONSULTA_DIA(int LIMITE_CONSULTA_DIA) {
+        this.LIMITE_CONSULTA_DIA = LIMITE_CONSULTA_DIA;
+    }
+
+    public String getENVIO_ROL() {
+        return ENVIO_ROL;
+    }
+
+    public void setENVIO_ROL(String ENVIO_ROL) {
+        this.ENVIO_ROL = ENVIO_ROL;
+    }
+
+    public String getTOTAL_HORAS() {
+        return TOTAL_HORAS;
+    }
+
+    public void setTOTAL_HORAS(String TOTAL_HORAS) {
+        this.TOTAL_HORAS = TOTAL_HORAS;
+    }
+
+    public Double getTOTAL_PAGO() {
+        return TOTAL_PAGO;
+    }
+
+    public void setTOTAL_PAGO(Double TOTAL_PAGO) {
+        this.TOTAL_PAGO = TOTAL_PAGO;
+    }
+
+    public String getNOM_USU() {
+        return NOM_USU;
+    }
+
+    public void setNOM_USU(String NOM_USU) {
+        this.NOM_USU = NOM_USU;
+    }
+  
 }
